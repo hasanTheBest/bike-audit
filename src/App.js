2855ad43f455c7cc3bdc2e1cdd71390fc0793724
hasanTheBest <rainbowmahmud@gmail.com>
2022-04-04 15:13:01 +0600

    initial setup with react-router, tailwind-css

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import NotFound from "./components/404/NotFound";
+import Blog from "./components/Blog/Blog";
+import Dashboard from "./components/Dashboard/Dashboard";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Reviews from "./components/Reviews/Reviews";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/reviews" element={<Reviews />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </main>
+      <footer>
+        &copy; <a href="https://github.com/hasanTheBest">Mahmud</a> | All rights
+        reserved
+      </footer>
+    </>
   );
 }
 
